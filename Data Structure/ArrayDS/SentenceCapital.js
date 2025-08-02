1cea8093e912788d0edd2501b8816bb803f5a7ec
Mohd Wasim <pydwasim@gmail.com>
2025-08-02 14:11:45 +0530

    add 2 files

---
+++ Data Structure/ArrayDS/SentenceCapital.js
@@ -0,0 +1,4 @@
+const sentenceCapital = sentence => { 
+    return sentence.toLowerCase().split(" ").map((word) => word[0].toUpperCase() + word.slice(1)).join(" ");
+}
+console.log(sentenceCapital("who is the be"))
